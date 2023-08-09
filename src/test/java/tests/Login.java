63c404b576c8923dbab1cc81520add0a0b03e769
Shelly Mutu-Grigg <shellymutugrigg@gmail.com>
2023-08-10 08:54:18 +1200

    BUILD: Updated for amazon.com

--- src/test/java/tests/Login.java
+++ src/test/java/tests/Login.java
@@ -30,8 +30,8 @@ public class Login {
 		properties.load(fileInputStream);
 		String url = System.getProperty("url") != null ? System.getProperty("url")
 		: properties.getProperty("url");
-		String legalText = System.getProperty("legalText") != null ? System.getProperty("legalText")
-				: properties.getProperty("legalText");
+		String titleText = System.getProperty("titleText") != null ? System.getProperty("titleText")
+				: properties.getProperty("titleText");
 		
 		// Load .env file variables
 		Dotenv dotenv = Dotenv
@@ -40,35 +40,35 @@ public class Login {
 				   .load();
 		String email = dotenv.get("EMAIL");
 		String password = dotenv.get("PASSWORD");
-		
-		System.out.println(MessageFormat.format("URL: {0}", url));
-		System.out.println(MessageFormat.format("Email: {0}", email));
-		System.out.println(MessageFormat.format("websiteTitle: {0}", legalText));
 
-		// Navigate to web page
 		webDriver.manage().window().maximize();
 		webDriver.get(url);
 		
 		WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ng-star-inserted")));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".nav-progressive-attribute")));
 		
-		String legal = webDriver.findElement(By.cssSelector(".legalBar-highlight")).getText();
-		System.out.println(MessageFormat.format("legalText: {0}", legal));
-		Assert.assertEquals(legal, legalText);
+		String title = webDriver.getTitle();
+		System.out.println(MessageFormat.format("Page Title: {0}",title));
+		Assert.assertEquals(title, titleText);
 		
-		webDriver.findElement(By.cssSelector("a[title='Sign In'] span[class='ng-star-inserted']")).click();
+		webDriver.findElement(By.xpath("//span[normalize-space()='Account & Lists']")).click();
 		
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ng-tns-c88-0")));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".a-spacing-small")));
 		
-		webDriver.findElement(By.id("emailInput")).sendKeys(email);
-		webDriver.findElement(By.cssSelector("button[value='Submit'")).click();	
+		webDriver.findElement(By.id("ap_email")).sendKeys(email);
+		webDriver.findElement(By.id("continue")).click();	
 		
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".text-wrapper")));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".a-form-label")));
 		
-		webDriver.findElement(By.id("password")).sendKeys(password);
-		webDriver.findElement(By.cssSelector("button[value='Submit'")).click();	
+		webDriver.findElement(By.id("ap_password")).sendKeys(password);
+		webDriver.findElement(By.id("signInSubmit")).click();	
 		
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ng-tns-c148-3")));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".nav-logo-link")));
+		
+//		webDriver.findElement(By.id("password")).sendKeys(password);
+//		webDriver.findElement(By.cssSelector("button[value='Submit'")).click();	
+//		
+//		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ng-tns-c148-3")));
 		
 	}
 
